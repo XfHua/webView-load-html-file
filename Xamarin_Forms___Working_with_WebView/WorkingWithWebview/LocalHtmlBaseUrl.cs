e1f8e06da13562ad5c6346b5a4ad4d87e1c4811a
XfHua <jasonHuaxf@163.com>
2019-11-27 11:26:15 +0800

    Add files via upload

---
+++ Xamarin_Forms___Working_with_WebView/WorkingWithWebview/LocalHtmlBaseUrl.cs
@@ -0,0 +1,36 @@
+﻿using Xamarin.Forms;
+
+namespace WorkingWithWebview
+{
+    public interface IBaseUrl { string Get(); }
+
+    public class LocalHtmlBaseUrl : ContentPage
+    {
+        public LocalHtmlBaseUrl()
+        {
+            var browser = new WebView();
+
+
+            var htmlSource = new HtmlWebViewSource();
+
+            htmlSource.Html = @"<html>
+                                <head>
+                                <link rel=""stylesheet"" href=""default.css"">
+                                </head>
+                                <body>
+                                <h1>click next page to watch the video</h1>
+                                <p><a href=""local.html"">next page</a></p>
+                                </body>
+                                </html>";
+
+            htmlSource.BaseUrl = DependencyService.Get<IBaseUrl>().Get();
+            browser.Source = htmlSource;
+            Content = browser;
+        }
+    }
+
+    public class myWeb : WebView { 
+    
+
+    }
+}
---
+++ Xamarin_Forms___Working_with_WebView/iOS/MyWebRenderer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using UIKit;
+using WebKit;
+using WorkingWithWebview;
+using WorkingWithWebview.iOS;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer (typeof(myWeb), typeof(MyWebRenderer))]
+namespace WorkingWithWebview.iOS
+{
+    public class MyWebRenderer : WebViewRenderer
+    {
+        protected override void OnElementChanged(VisualElementChangedEventArgs e)
+        {
+            base.OnElementChanged(e);
+
+            //this.MediaPlaybackRequiresUserAction = false;
+        }
+    }
+}
